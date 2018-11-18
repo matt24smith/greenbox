341473bdb2146159aeeb4dfd97ed131d72166478
matt smith <matt24smith@gmail.com>
2018-11-18 18:05:32 -0400

    fixing front end file paths

--- var/www/html/projects/greenbox/greenbox.js
+++
@@ -1,2 +0,0 @@
-var imgtag = "<img src='graph.png?1542574544' alt='Sensor Data (24h)'></img>"
-document.getElementById('graphImg').innerHTML = imgtag
--- var/www/html/projects/greenbox/json.js
+++
@@ -1,1 +0,0 @@
-jsonData = JSON.stringify([{'celsius': 22.5, 'humidity': 40.2, 'heat index': 40.0, 'ph': 5.8, 'ec': 1.5, 'reservoir': 21.375, 'ph raw voltage': 0.797754}]);
